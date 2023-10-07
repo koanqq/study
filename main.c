0b3648bae5e780fa4804516c6024a02cc0783a9c
Koan <kncocororo@gmail.com>
2023-10-08 00:07:44 +0800

    initial

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    printf("input test1:"); 
+    scanf("%d", &n);
+    printf("Test1!n=%d, c=%c\n", n, n);
+    return 0;
+}
+
+// gcc main.c -o main  
